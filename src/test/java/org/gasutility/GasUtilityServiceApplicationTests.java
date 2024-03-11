bdeadc296f3895a84c6a65e3e7a099071df2e2b9
sachin01k <sachinkamble825@gmail.com>
2024-03-11 23:27:24 +0530

    first commit

---
+++ src/test/java/org/gasutility/GasUtilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gasutility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasUtilityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
